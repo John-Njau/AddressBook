9124080dcdd939b605059cb009dd163ae44c9211
John <john.njoroge@student.moringaschool.com>
2022-03-18 13:11:08 +0300

    ft: Added a button to call another address form input fields

--- js/script.js
+++ js/script.js
@@ -5,22 +5,37 @@
 //     this.lastname = lastname;
 //     this.phoneNo = phoneNo;
 // }
+
+
+
 // contact constructor
-function Contact(first, last, email, city) {
+function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
-    this.emailAddress = email;
-    this.city = city;
+    this.addresses = [];
+}
+
+// address constructor
+function Address(street, city, county) {
+    this.streetName = street;
+    this.cityName = city;
+    this.countyName = county;
 }
 
 // Adding a full name prototype
-Contact.prototype.fullName = function(){
+Contact.prototype.fullName = function () {
     return this.firstName + " " + this.lastName;
 }
 
-// var newP = new Contact('john', 'kim');
-// console.log(newP);
-
+// Contact object and an Address object
+var newP = new Contact('john', 'kim');
+var home = new Address("671 Ngong Road", "Nairobi", "Nairobi");
+console.log(newP);
+console.log(home);
+newP.addresses.push(home);
+console.log(newP);
+console.log(newP.addresses[0]);
+console.log(newP.addresses[0].cityName);
 // jQuery
 // user Interface Logic
 $().ready(function () {
@@ -34,7 +49,7 @@ $().ready(function () {
 
         var newContact = new Contact(firstNameInput, lastNameInput, emailInput, city);
 
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() +  "<br>" + emailInput + city + "</span></li>");
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "<br>" + emailInput + city + "</span></li>");
 
         $(".contact").last().click(function () {
             $("#showContact").show();
@@ -46,3 +61,21 @@ $().ready(function () {
         $("input#newLastName").val("");
     });
 });
+
+$().ready(function () {
+    $("#addAddress").click(function () {
+        $("#newAddresses").append('<div class="newAddress">' + '<div class="form-group">' +
+            '<label for="newStreet">Street</label>' +
+            '<input type="text" class="form-control newStreet">' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label for="newCity">City</label>' +
+            '<input type="text" class="form-control newCity">' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label for="newCounty">County</label>' +
+            '<input type="text" class="form-control newCounty">' +
+            '</div>' +
+            '</div>');
+    });
+})
